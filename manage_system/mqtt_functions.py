7d00632e9128ab3d2b66a0a983b0d39336a666a3
lrqq <2312652727@qq.com>
2022-08-13 05:11:36 -0400

    mqtt微调

--- manage_system/mqtt_functions.py
+++ manage_system/mqtt_functions.py
@@ -1,6 +1,7 @@
 # 为了能在外部脚本中调用Django ORM模型，必须配置脚本环境变量，将脚本注册到Django的环境变量中
 from email import message
 import os, sys
+import time
 import django
 # 引入mqtt包
 import paho.mqtt.client as mqtt
@@ -17,7 +18,7 @@ django.setup()
 
 # 建立mqtt连接
 def on_connect(client, userdata, flag, rc):
-    # print("Connect with the result code " + str(rc))
+    print("Connect with the result code " + str(rc))
     client.subscribe('total_weight', qos=2)
 
 # 接收、处理mqtt消息
@@ -26,29 +27,34 @@ def on_message(client, userdata, msg):
     global_var.set_value("msg", out)
     print(out)
 
+def on_disconnect(client, userdata, rc):
+    print("Disonnect with the result code " + str(rc))
 
 
 # mqtt客户端启动函数
 def mqttfunction():
     global client
     # 使用loop_start 可以避免阻塞Django进程，使用loop_forever()可能会阻塞系统进程
-    client.loop_start()
+    #client.loop_start()
     # client.loop_forever() 有掉线重连功能
-    # client.loop_forever(retry_first_connection=True)
+    client.loop_forever(retry_first_connection=True)
 
 
-client = mqtt.Client(client_id="test", clean_session=False)
+client = mqtt.Client(client_id='client-f{random.randint(0, 1000)}', clean_session=False)
 
 
 # 启动函数
 def mqtt_run():
     client.on_connect = on_connect
     client.on_message = on_message
+    client.on_disconnect = on_disconnect
     # 绑定 MQTT 服务器地址
     # broker = '192.168.31.228'
+    broker = "mqtt.eclipseprojects.io"
     # MQTT服务器的端口号
     client.connect(mqttBroker)
     # 启动
+    #client.loop_start()
     mqttthread = Thread(target=mqttfunction)
     mqttthread.start()
 
